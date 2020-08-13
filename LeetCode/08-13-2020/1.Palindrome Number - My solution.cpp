4b469a311183f2c41a734537dd4ed2186d9f41d8
Kapadia Karan Anupbhai <60576483+Karan-kapadia@users.noreply.github.com>
2020-08-13 23:45:18 +0530

    All the code that I solved on LeetCode

---
+++ LeetCode/08-13-2020/1.Palindrome Number - My solution.cpp
@@ -0,0 +1,14 @@
+// Problem name - Palindrome Number
+
+// My solution
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string str = to_string(x);
+        for(int i=0, j=str.size()-1; i<=j; i++, j--){
+            if(str[i] != str[j])
+                return false;
+        }
+        return true;
+    }
+};
---
+++ LeetCode/08-13-2020/2. Fibonacci_number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fib(int N) {
+        if(N == 0)
+            return N;
+        else if(N == 1)
+            return N;
+        int * arr = new int[N+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for(int i=2; i<=N; i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        int output = arr[N];
+        delete [] arr;
+        return output;
+    }
+};
